29e43935e2bb46ede358d0c629f27ef7e0c80ef5
tresjuess <113621221+tresjuess@users.noreply.github.com>
2023-03-03 10:33:38 +0300

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,1 @@
-print("Good bye world, you are so cruel, I hate you. The world doesn't need me anymore. I'm leaving.")
+print("Good bye world, you are so cruel, I hate you!!! The world doesn't need me anymore. I'm leaving.")
